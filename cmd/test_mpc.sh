0310c384703f9e786fe52ef984b4f629d01d0aa5
Hu Shili <799498265@qq.com>
2025-08-07 15:49:25 +0800

    add beacon input to test script

--- cmd/test_mpc.sh
+++ cmd/test_mpc.sh
@@ -3,5 +3,5 @@ go run mpccmd.go CommonSRS init --inner-ccs inner_ccs_7 --srs srs_1
 go run mpccmd.go CommonSRS checkInit --inner-ccs inner_ccs_7 --srs srs_1
 go run mpccmd.go CommonSRS contribute --inner-ccs inner_ccs_7 --input srs_1 --output srs_2
 go run mpccmd.go CommonSRS verify --inner-ccs inner_ccs_7 --input srs_1 --output srs_2
-go run mpccmd.go export innerSeal --srs srs_2 --inner-ccs inner_ccs_ --inner-pk inner_pk_ --inner-vk inner_vk_
-go run mpccmd.go export outerSeal --srs srs_2 --inner-ccs inner_ccs_ --inner-pk inner_pk_ --inner-vk inner_vk_
+go run mpccmd.go export innerSeal --srs srs_2 --beacon test --inner-ccs inner_ccs_ --inner-pk inner_pk_ --inner-vk inner_vk_
+go run mpccmd.go export outerSeal --srs srs_2 --beacon test --inner-ccs inner_ccs_ --inner-pk inner_pk_ --inner-vk inner_vk_
